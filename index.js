2758f30e090a735d052f79ce141ac13b79f1eb30
Yuratama1 <yuratama661@gmail.com>
2026-07-15 09:57:36 +0700

    Membuat PUT untuk memperbarui data bedasarkan ID

--- index.js
+++ index.js
@@ -54,6 +54,30 @@ app.post('/', (req, res) => {
         });
 })
 
+// Membuat PUT untuk memperbarui data bedasarkan ID
+app.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { nama, nim, kelas } = req.body;
+
+    pool.query(
+        'UPDATE biodata SET nama = $1, nim = $2, kelas = $3 WHERE id = $4 RETURNING *',
+        [nama, nim, kelas, id]
+    )
+        .then(result => {
+            if (result.rows.length === 0) {
+                return res.status(404).send("Data tidak ditemukan");
+            }
+            res.json({
+                message: 'Data biodata berhasil diperbarui',
+                data: result.rows[0]
+            });
+        })
+        .catch(err => {
+            console.error("Error executing query", err.stack);
+            res.status(500).send("Database Error");
+        });
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
