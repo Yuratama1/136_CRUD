c9638f1a180ad7a641e0c6745bf67553cef0bea0
Yuratama1 <yuratama661@gmail.com>
2026-07-15 09:58:50 +0700

    Membuat DELETE untuk menghapus data bedasarkan ID

--- index.js
+++ index.js
@@ -78,6 +78,29 @@ app.put('/:id', (req, res) => {
         });
 });
 
+// Membuat DELETE untuk menghapus data bedasarkan ID
+app.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    pool.query(
+        'DELETE FROM biodata WHERE id = $1 RETURNING *',
+        [id]
+    )
+        .then(result => {
+            if (result.rows.length === 0) {
+                return res.status(404).send("Data tidak ditemukan");
+            }
+            res.json({
+                message: 'Data biodata berhasil dihapus',
+                data: result.rows[0]
+            });
+        })
+        .catch(err => {
+            console.error("Error executing query", err.stack);
+            res.status(500).send("Database Error");
+        });
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
